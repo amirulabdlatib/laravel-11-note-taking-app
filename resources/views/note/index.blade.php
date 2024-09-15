423e2949b1b4671c0b7b516272491052b53078a5
Amirul Abd Latib <amirul.latib@alphv.com>
2024-09-15 16:28:55 +0800

    Add index page and update skeletal note controller

---
+++ resources/views/note/index.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    Index page
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\NoteController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,4 @@ Route::get('/', function () {
 
 Route::get('/login',[AuthController::class,'getLoginPage'])->name('login.page');
 Route::get('/register',[AuthController::class,'getRegisterPage'])->name('register.page');
+Route::resource('notes',NoteController::class);
