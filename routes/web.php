e30833be1fe7c8965050b2e30612bb515511a97b
Amirul Abd Latib <amirul.latib@alphv.com>
2024-09-15 16:00:17 +0800

    Add login and register page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/login',[AuthController::class,'getLoginPage'])->name('login.page');
+Route::get('/register',[AuthController::class,'getRegisterPage'])->name('register.page');
