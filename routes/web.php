86f1996b5d7e470488bc5f7decd19630cbb3a68e
Amirul Abd Latib <amirul.latib@alphv.com>
2024-09-15 21:43:35 +0800

    Add custom middleware

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,10 @@ use App\Http\Controllers\NoteController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
 Route::get('/login',[AuthController::class,'getLoginPage'])->name('login.page');
 Route::post('/login',[AuthController::class,'login'])->name('post.login');
 Route::post('/logout',[AuthController::class,'logout'])->name('post.logout');
 Route::get('/register',[AuthController::class,'getRegisterPage'])->name('register.page');
 Route::post('/register',[AuthController::class,'register'])->name('post.register');
 
-Route::resource('notes',NoteController::class);
+Route::resource('notes',NoteController::class)->middleware('loggedIn');
