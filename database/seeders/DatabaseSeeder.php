e73fe38b4f181a49f43cb00ba20e76a6975ce02e
Amirul Abd Latib <amirul.latib@alphv.com>
2024-09-15 16:18:23 +0800

    Add model,migration,factory,seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Amirul',
+            'email' => 'amirul@example.com',
+        ]);
+
+        User::factory(3)->create();
+
+        $this->call([
+            NoteSeeder::class
         ]);
     }
 }
